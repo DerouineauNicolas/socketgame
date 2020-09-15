860c23dde9b3242f29b6474b2379190e3715406c
Nicolas DEROUINEAU <nicodero72@gmail.com>
2020-09-15 13:26:28 +0200

    Handle disconnection

--- server/server.js
+++ server/server.js
@@ -10,18 +10,34 @@ var state = {
   Players: [],
 };
 
+function getUniqueID() {
+  function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+  }
+  return s4() + s4() + '-' + s4();
+};
+
 
 wss.on('connection', function connection(ws) {
-  var player = new game.Player(1, "toto");
+  ws.id = getUniqueID();
+  var player = new game.Player(ws.id, ws.id);
   state.Players.push(player);
   ws.on('message', function incoming(message) {
     console.log('received: %s', message);
   });
 
+  ws.onclose = function(e) {
+    console.log("close");
+    state.Players.forEach(function(player, index, object){
+      if (player.id === ws.id){
+        object.splice(index, 1);
+      }
+    });
+  };
+
   setInterval(function() {
-    console.log(state);
     ws.send(JSON.stringify(state));
-  }, 2000);
+  }, 500);
  
 });
 
