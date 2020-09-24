4e3602780d5edf6f41100eae8fcf16348c8ff39e
Nicolas DEROUINEAU <nicodero72@gmail.com>
2020-09-24 10:36:37 +0200

    Add lights

--- client/src/Viewer.js
+++ client/src/Viewer.js
@@ -4,7 +4,7 @@ import * as THREE from 'three'
 var oldplayerset = new Set();
 var scene;
 var gamestate = [];
-var num_player = 0;
+var num_player = 1;
 
 
 function initGameContext(mount)
@@ -21,9 +21,21 @@ function initGameContext(mount)
 
   var planegeometry = new THREE.PlaneGeometry( 50, 20, 32 );
 
-  var planematerial = new THREE.MeshBasicMaterial( {color: 0x0000ff, side: THREE.DoubleSide} );
+  var planematerial = new THREE.MeshLambertMaterial({
+    color: 0x00ff00,
+    side: THREE.DoubleSide
+  });
   
   var plane = new THREE.Mesh( planegeometry, planematerial );
+
+
+  var directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5 );
+  directionalLight.position.set(0, 0, 5);
+  scene.add( directionalLight );
+
+  var directionalLight2 = new THREE.DirectionalLight( 0xffffff, 0.5);
+  directionalLight2.position.set(-20, -50, 50);
+  scene.add( directionalLight2 );  
 
 
   scene.add(plane);
@@ -32,7 +44,7 @@ function initGameContext(mount)
   camera.position.y = -50;
   camera.up.set( 0, 0, 1 );
   camera.lookAt(0, 0, 0);
-  renderer.setClearColor('#000000')
+  //renderer.setClearColor('#000000')
   renderer.setSize(width, height)
 
   const renderScene = () => {
@@ -56,7 +68,7 @@ function initGameContext(mount)
         var mesh = scene.getObjectByName( player.name)
         if (!mesh){
           const geometry = new THREE.BoxGeometry(1, 1, 1)
-          const material = new THREE.MeshBasicMaterial({ color: ((255.0)/(10.0)) * num_player * 0xffffff })
+          const material = new THREE.MeshLambertMaterial({ color: ((255.0)/(10.0)) * num_player * 0xffffff })
           var cube = new THREE.Mesh( geometry, material );
           cube.name = player.name;
           cube.position.set( player.x, player.y, 1 );
@@ -89,7 +101,7 @@ function initGameContext(mount)
         var mesh = scene.getObjectByName( point.id)
         if (!mesh){
           const geometry = new THREE.BoxGeometry(1, 1, 1);
-          const material = new THREE.MeshBasicMaterial({ color:0xff0000} );
+          const material = new THREE.MeshLambertMaterial({ color:0xff0000} );
           var cube = new THREE.Mesh( geometry, material );
           cube.name = point.id;
           cube.position.set( point.x, point.y, 1 );
